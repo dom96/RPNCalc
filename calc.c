73573d864b8597d63edeb7f84219745ee2e898c6
dom96 <dominikpicheta@googlemail.com>
2010-06-09 23:01:35 +0100

    Made it very minimalistic.

--- calc.c
+++ calc.c
@@ -1,89 +1,40 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <ctype.h>
-#include "stack.h"
-
-bool popE(stack *s, T *item); // Prototype
 
 int main()
 {
   printf("Reverse Polish Notation Calculator v0.1 by dom96\n\n");
   while (1)
   {
-    /* Every time a new calculation is executed, a new stack is created. */
-    stack *dataStack = new();
-  
-    char input[1024];
-    gets(input);
-
-    /* Loop through the whole string */
-    int i;
-    for( i = 0; i < strlen(input); i++ )
+    
+    /* Get two INT's */
+    int first, second;
+    if(scanf("%i", &first) == 1 && scanf("%i", &second))
     {
-      /* If this character is a digit, push it on the stack */
-      if (isdigit(input[i]))
+      /* Get the operator */
+      char op[2];
+      if(scanf("%1s", op) == 1)
       {
-        push(dataStack, atoi(&input[i]));
-      }
-      else if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/')
-      {
-        //printf("Received, operator: %c\n", input[i]);
-        T first;
-        T second;
-        if (popE(dataStack, &first) && popE(dataStack, &second))
+        switch(op[0])
         {
-          switch(input[i])
-          {
-            case '+':
-            {
-              printf("== %i\n", first + second);
-              break;
-            }
-            case '-':
-            {
-              printf("== %i\n", first - second);
-              break;
-            }
-            case '*':
-            {
-              printf("== %i\n", first * second);
-              break;
-            }
-            case '/':
-            {
-              printf("== %i\n", (int) first / second);
-              break;
-            }
-          }
-        }
-        else
-        {
-          printf("Error: Not enough values in the stack.\n");
+          case '+': printf("== %i\n", first + second); break;
+          case '-': printf("== %i\n", first - second); break;
+          case '*': printf("== %i\n", first * second); break;
+          case '/': printf("== %i\n", first / second); break;
+          default: printf("Error: Invalid operator.\n");
         }
       }
       else
       {
-        if (input[i] != ' ')
-        {
-          printf("Unsupported operator: %c\n", input[i]);
-        }
+        printf("Error: Invalid operator.\n");
       }
+    }
+    else
+    {
+      printf("Error: Number expected.\n");
+      break;
     }
   }
 }
 
-bool popE(stack *s, T *item)
-/* Pops a value from the stack and sets item to the value popped. Returns False if that failed and true if it didn't. */
-{
-  if (!empty(s))
-  {
-    *item = pop(s);
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-}
 
+ 
